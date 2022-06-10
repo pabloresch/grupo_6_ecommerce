3f84c452ee9d3706e51fe9475e2804d03716a7b7
PeterSauma <pedrosaumacammi@gmail.com>
2022-06-09 21:40:07 -0300

    se trabajo en las rutas post de creacion de produccto y get de detalle de producto, sin éxito. Tambien se modificó el estilo visual del formulario de creacion de producto

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,3 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
+
 const controllers = {
     /*detalleProducto : (req, res) => {
         if (req.params.idProducto == undefined) {
@@ -10,17 +19,48 @@ const controllers = {
     carrito : (req, res) => {
         res.render("productCart")
     },
-    productDetail : (req, res) => {
+    /*productDetail : (req, res) => {
         res.render('productDetail')
     },
 
     listaCelulares :(req, res)=> {
         res.render('listPhones')
-    },
+    },*/
 
     newProduct: (req, res) => {
         res.render('newProduct')
-    }
+    },
+    // Create -  Method to store
+        store: (req, res) => {
+        let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+        let newProduct = {
+            id: products[products.length - 1].id + 1,
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            discount: req.body.discount,
+            category: req.body.category,
+            image: req.file.filename,
+            section: req.body.section,
+            marca: req.body.marca
+        };
+        products.push(newProduct);
+
+        let newProductSave = JSON.stringify(products, null, 2);
+        fs.writeFileSync(productsFilePath, newProductSave, "utf-8");
+
+        return res.redirect("/");
+        },
+        detail: (req, res) => {
+            const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+        
+            let id = req.params.id;
+        
+            let detailProduct = products.find((prod) => prod.id == id);
+        
+            return res.render("productDetail", { detailProduct });
+          },
 }
 
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,19 +1,37 @@
+// ************ Require's ************
 const express = require ('express');
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+       cb(null, './public/images/products'); 
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+  })
+
+const upload = multer({ storage });
+
 const productControllers = require ('../controllers/productControllers')
 
 
+/*router.get("listado/:idProducto?", productControllers.detalleProducto );*/
 
-router.get("/productDetail", productControllers.productDetail );
+
 
 router.get("/productCart", productControllers.carrito );
 
 router.get("/listPhones", productControllers.listaCelulares );
 
-/*router.get("listado/:idProducto?", productControllers.detalleProducto );*/
+/*** GET ONE PRODUCT ***/
+//router.get("/:id", productControllers.detail);
+router.get("/detail/:id", productControllers.detail );
 
+/*** CREATE ONE PRODUCT ***/
 router.get('/newProduct', productControllers.newProduct)
-
+router.post("/", upload.single("imgFile"), productControllers.store);
 
 module.exports =router
 
